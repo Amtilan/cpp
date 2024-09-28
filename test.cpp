3b30f987efdab2adf320a693204da29dd962cd69
Amtilan <149930972+Amtilan@users.noreply.github.com>
2024-09-28 23:07:45 +0500

    ads flex

--- test.cpp
+++ test.cpp
@@ -1,10 +1,17 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
-    cout << "Hello World!" << endl;
-    cout << 2 + 2;
+    int n; 
+    cin >> n;
+    while(n != 1){
+        if(n % 2 != 0){
+            cout << "NO" << endl;
+            break;
+        }
+        n /= 2;
+    }
+    if(n == 1) cout= << "YES" << endl;
     return 0;
-    cout << n % 180;
 }
